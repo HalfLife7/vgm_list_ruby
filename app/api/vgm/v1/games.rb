8589bcb231dcb1c466d92ff11903112d4ec904d0
HalfLife7 <d.nguyen@vehikl.com>
2023-03-29 20:55:51 -0400

    add /games endpoint

---
+++ app/api/vgm/v1/games.rb
@@ -0,0 +1,92 @@
+module Vgm
+  module V1
+    class Games < Vgm::API
+      helpers Vgm::V1::Helpers
+      resources :games do
+        # GET /api/v1/games
+        desc 'Return all games'
+        params do
+          optional :limit, type: Integer, desc: 'limit results', default: 5
+        end
+        get do
+          games = Game.includes(
+            :game_alternative_names,
+            :game_artworks,
+            :collections,
+            :game_covers,
+            :platforms,
+            :game_screenshots,
+            :game_videos,
+            :game_websites,
+            :albums
+          ).limit(params[:limit])
+          status 200
+          present games, with: Entities::Game
+        rescue StandardError => e
+          error!(e)
+        end
+
+        # GET /api/v1/games/:id
+        desc 'Returns specific game'
+        route_param :id, type: Integer do
+          get do
+            status 200
+            Game.find(params[:id])
+          rescue ActiveRecord::RecordNotFound
+            error!('Record not found', 404)
+          end
+        end
+
+        # POST /api/v1/games
+        desc 'Create a game'
+        params do
+          requires :name, type: String, allow_blank: false
+          optional :aggregated_rating_count, type: Integer
+          optional :aggregated_rating, type: Float
+          optional :category, type: String
+          optional :first_release_date, type: Date
+          optional :summary, type: String
+        end
+        post do
+          game = Game.create!(params)
+          status 200
+          { game: game, message: 'Game created successfully' }
+        rescue StandardError => e
+          error!(e)
+        end
+
+        # PATCH /api/v1/games/:id
+        params do
+          optional :name, type: String, allow_blank: false
+          optional :aggregated_rating_count, type: Integer
+          optional :aggregated_rating, type: Float
+          optional :category, type: String
+          optional :first_release_date, type: Date
+          optional :summary, type: String
+        end
+        route_param :id, type: Integer do
+          patch do
+            game = Game.find(params[:id])
+            game.update(params)
+            status 200
+            { game: game, message: 'Game updated successfully' }
+          rescue StandardError => e
+            error!("Invalid - #{e}", 400)
+          end
+        end
+
+        # DELETE /api/v1/games/:id
+        route_param :id, type: Integer do
+          delete do
+            game = Game.find(params[:id])
+            game.delete
+            status 200
+            { message: 'Game deleted successfully' }
+          rescue StandardError => e
+            error!(e)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/requests/games_spec.rb
@@ -0,0 +1,147 @@
+require 'rails_helper'
+
+RSpec.describe 'Games', type: :request do
+  describe 'GET /games' do
+    let!(:game) { FactoryBot.create(:game) }
+
+    it 'returns success code' do
+      get '/api/v1/games'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns all games' do
+      get '/api/v1/games'
+      expect(JSON.parse(response.body)).to include_json([{
+                                                          id: game.id, name: game.name
+                                                        }])
+    end
+
+    context 'when it has query params' do
+      context 'when limiting the number of games returned' do
+        let(:query_params) { { limit: 3 } }
+        before { FactoryBot.create_list(:game, 6) }
+
+        it 'returns 10 results' do
+          get '/api/v1/games', params: query_params
+          expect(JSON.parse(response.body).size).to eq(3)
+        end
+      end
+    end
+  end
+
+  describe 'get /games/:id' do
+    let!(:game) { FactoryBot.create(:game) }
+
+    it 'returns 404 for invalid id' do
+      get '/api/v1/games/99999'
+      expect(response).to have_http_status(404)
+    end
+
+    it 'returns a specific game' do
+      get "/api/v1/games/#{game.id}"
+      expect(JSON.parse(response.body)).to eq(JSON.parse(game.to_json))
+    end
+  end
+
+  describe 'post /games' do
+    subject(:post_game) { post '/api/v1/games', params: params }
+    let!(:params) do
+      {
+        name: Faker::Game.title,
+        category: Game.categories.keys.sample(1).first,
+        summary: Faker::Lorem.sentences.join(' ')
+      }
+    end
+
+    context 'valid params' do
+      it 'creates a new game' do
+        expect { post_game }.to change { Game.all.size }.by 1
+        expect(Game.all.first['name']).to eq(params[:name])
+      end
+
+      it 'returns a 200 status' do
+        post_game
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'invalid params' do
+      it 'returns name is empty error' do
+        params[:name] = ''
+        post_game
+        expect(JSON.parse(response.body)['error']).to eq('name is empty')
+        expect(response.status).to eq(400)
+      end
+
+      it 'returns empty name error' do
+        params.delete(:name)
+        post_game
+        expect(JSON.parse(response.body)['error']).to eq('name is missing, name is empty')
+        expect(response.status).to eq(400)
+      end
+    end
+  end
+
+  describe 'patch /games/:id' do
+    subject(:patch_game) { patch "/api/v1/games/#{game.id}", params: params }
+    let!(:game) { FactoryBot.create(:game) }
+
+    context 'valid params' do
+      let(:params) { { summary: 'This is an updated summary.' } }
+      it 'updates the game\'s summary' do
+        expect { patch_game }.to change { Game.find(game.id)['summary'] }.to(params[:summary])
+      end
+
+      it 'returns a success response' do
+        patch_game
+        expect(JSON.parse(response.body)['message']).to eq('Game updated successfully')
+      end
+    end
+
+    context 'invalid params' do
+      context 'name is blank' do
+        let(:params) do
+          {
+            name: ''
+          }
+        end
+        it 'returns an error message' do
+          patch_game
+          expect(JSON.parse(response.body)['error']).to eq('name is empty')
+        end
+      end
+    end
+  end
+
+  describe 'delete /games/:id' do
+    subject(:delete_game) { delete "/api/v1/games/#{delete_id}" }
+    let!(:game) { FactoryBot.create(:game) }
+    context 'valid params' do
+      let(:delete_id) { game.id }
+      it 'deletes the game' do
+        expect { delete_game }.to change { Game.all.size }.from(1).to(0)
+      end
+
+      it 'returns a success message' do
+        delete_game
+        expect(JSON.parse(response.body)['message']).to eq('Game deleted successfully')
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'invalid params' do
+      let(:delete_id) { game.id + 1 }
+
+      it 'returns an error message' do
+        delete_game
+        expect(JSON.parse(response.body)['error']).to eq("Couldn't find Game with 'id'=#{delete_id}")
+      end
+
+      it 'returns a 500' do
+        delete_game
+        expect(response.status).to eq(500)
+      end
+    end
+  end
+end
